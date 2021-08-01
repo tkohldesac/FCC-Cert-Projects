ce9b4bf063d075a0eaa3eeea6227a2a3aea0ed2b
tkohldesac <topher@kohldesac.com>
2021-08-01 16:13:20 -0700

    js

---
+++ Scripts/relics.js
@@ -0,0 +1,10 @@
+const powderValue = document.getElementById("currentPowderTotal").innerText;
+const torturedValue = document.getElementById("torturedCurrentTotal");
+const harrowingValue = document.getElementById("harrowingCurrentTotal");
+const sorrowfulValue = document.getElementById("sorrowfulCurrentTotal");
+const bitterValue = document.getElementById("bitterCurrentTotal");
+const loathsomeValue = document.getElementById("loathsomeCurrentTotal");
+const timewornValue = document.getElementById("timewornCurrentTotal");
+const rawValue = document.getElementById("rawCurrentTotal");
+
+console.log("powderValue")
