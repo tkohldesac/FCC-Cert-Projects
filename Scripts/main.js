b9132128edca076584b267722536b93c2de76a34
topher <topher@kohldesac.com>
2022-05-01 12:50:24 -0700

    Finished navbar for work, finished education formatting, fixed formatting for mobile

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,4 @@
-console.log("js is created");
-
-console.log("Hi");
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -18,6 +15,27 @@ function openCity(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the link that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openWork(evt, workName) {
+  // Declare all variables
+  var i, workContent, workLinks;
+
+  // Get all elements with class="workContent" and hide them
+  workContent = document.getElementsByClassName("workContent");
+  for (i = 0; i < workContent.length; i++) {
+    workContent[i].style.display = "none";
+  }
+
+  // Get all elements with class="workLinks" and remove the class "active"
+  workLinks = document.getElementsByClassName("workLinks");
+  for (i = 0; i < workLinks.length; i++) {
+    workLinks[i].className = workLinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(workName).style.display = "block";
   evt.currentTarget.className += " active";
 }
