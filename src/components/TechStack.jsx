9fe53c8a8983cca2fd3e223f30a9b5160393787b
Topher <topher@kohldesac.com>
2024-01-25 12:42:26 -0800

    lots of changes to BusinessCard

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,54 +1,53 @@
 import React, { } from 'react';
 import { Stack, Typography } from '@mui/material';
-import htmlSvgIcon from './helpers/images/svgs/html.svg';
-import cssSvgIcon from './helpers/images/svgs/css.svg';
-import { BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoReact, TbBrandReactNative, BiLogoPostgresql, BiLogoGit } from 'react-icons';
 
+import { BiLogoPostgresql, BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoReact, BiLogoGit, BiLogoTypescript  } from "react-icons/bi";
+import { TbBrandReactNative, TbBrandNodejs } from "react-icons/tb";
 
 export default function TechStack() {
 
 
     return (
         <>
-        {/* <img src={htmlSvgIcon} alt="html5" width="150" height="75"/> HTML5
-        <img src={cssSvgIcon} alt="html5" width="150" height="75"/> CSS3
-        <img src={htmlSvgIcon} width="150" height="75"/> JavaScript
-        <img src={htmlSvgIcon} width="150" height="75"/> React
-        <img src={htmlSvgIcon} width="150" height="75"/> React Native
-        <img src={htmlSvgIcon} width="150" height="75"/> PostgreSQL
-        <img src={htmlSvgIcon} width="150" height="75"/> Webpack
-        <img src={htmlSvgIcon} width="150" height="75"/> Git
-        <img src={htmlSvgIcon} width="150" height="75"/> NodeJS
-        <img src={htmlSvgIcon} width="150" height="75"/> TypeScript */}
+        <Stack direction="row">
+            <Stack alignItems='center' padding="1rem">
+                <BiLogoHtml5 size="5rem" />
+                <Typography >HTML5</Typography>
+            </Stack>
+            <Stack alignItems='center' padding="1rem">
+                <BiLogoCss3 size="5rem" />
+                <Typography alignItems='center'>CSS3</Typography>
+            </Stack>
+            <Stack alignItems='center' padding="1rem">
+                <BiLogoJavascript size="5rem" />
+                <Typography alignItems='center'>JavaScript</Typography>
+            </Stack>
+            <Stack alignItems='center' padding="1rem">
+                <BiLogoReact size="5rem" />
+                <Typography alignItems='center'>React</Typography>
+            </Stack>
+            <Stack alignItems='center' padding="1rem">
+                <TbBrandReactNative size="5rem" />
+                <Typography alignItems='center'>React Native</Typography>
+            </Stack>
+            <Stack alignItems='center' padding="1rem">
+                <BiLogoPostgresql size="5rem" />
+                <Typography alignItems='center'>PostgreSQL</Typography>
+            </Stack>
+            <Stack alignItems='center' padding="1rem">
+                <BiLogoGit size="5rem" />
+                <Typography alignItems='center' >Git</Typography>
+            </Stack>                
+            <Stack alignItems='center' padding="1rem">
+                <TbBrandNodejs size="5rem" />
+                <Typography alignItems='center' >NodeJS</Typography>
+            </Stack>
+            <Stack alignItems='center' padding="1rem">
+                <BiLogoTypescript  size="5rem" />
+                <Typography alignItems='center' >TypeScript</Typography>
+            </Stack>
+        </Stack>
 
-        <Stack alignItems='center' padding="1rem">
-                    <BiLogoHtml5 size="5rem" />
-                    <Typography >HTML5</Typography>
-                </Stack>
-                <Stack alignItems='center' padding="1rem">
-                    <BiLogoCss3 size="5rem" />
-                    <Typography alignItems='center'>CSS3</Typography>
-                </Stack>
-                <Stack alignItems='center' padding="1rem">
-                    <BiLogoJavascript size="5rem" />
-                    <Typography alignItems='center'>JavaScript</Typography>
-                </Stack>
-                <Stack alignItems='center' padding="1rem">
-                    <BiLogoReact size="5rem" />
-                    <Typography alignItems='center'>React</Typography>
-                </Stack>
-                <Stack alignItems='center' padding="1rem">
-                    <TbBrandReactNative size="5rem" />
-                    <Typography alignItems='center'>React Native</Typography>
-                </Stack>
-                <Stack alignItems='center' padding="1rem">
-                    <BiLogoPostgresql size="5rem" />
-                    <Typography alignItems='center'>PostgreSQL</Typography>
-                </Stack>
-                <Stack alignItems='center' padding="1rem">
-                    <BiLogoGit size="5rem" />
-                    <Typography alignItems='center' >Git</Typography>
-                </Stack>
         </>
 
      );
--- src/components/views/BusinessCard.jsx
+++ src/components/views/BusinessCard.jsx
@@ -1,49 +1,66 @@
 import React, { } from 'react';
-import profilepic from '../helpers/images/profilepic.png'
-import { Container, Link, Image, Flex, } from '@chakra-ui/react'
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
+// FIXME:
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { EmailIcon } from '@chakra-ui/icons'
+import EmailIcon from '@mui/icons-material/Email';
+
+import { 
+    Link, 
+    Grid, 
+    Box, 
+    Avatar, 
+    Accordion, 
+    AccordionSummary, 
+    AccordionDetails, 
+    Typography, 
+    Paper, 
+    styled 
+} from '@mui/material';
 
 import TechStack from '../TechStack.jsx';
+import profilepic from '../helpers/images/profilepic.png'
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 export default function BusinessCard() {
 
 
     return (
-        <Flex paddingTop={"50px"}>
-                <Container maxW="md" bg={"lightgray"}>
-                    <Typography variant="h4">Topher Kohlstedt</Typography>
-                        <Link href="http://www.kohldesac.dev">
-                            <Image src={profilepic} borderRadius='full' boxSize='300px' mr={2} verticalAlign={'middle'} />
-                        </Link>
-                    <Link 
-                        style={{ textDecoration: 'none', color:'black' }} 
-                        fontSize='2xl' 
-                        href="mailto:hello@kohldesac.com">
-                            <EmailIcon fontSize='xl'/> hello@kohldesac.com
-                            
+        <Box sx={{flexGrow:1}} margin="auto">
+        <Grid container bg={"lightgray"} m="auto" spacing={2}>
+            <Item item xs={4}>
+                <Typography variant="h4">Topher Kohlstedt</Typography>
+                    <Link href="http://www.kohldesac.dev">
+                        <Avatar src={profilepic} borderRadius='50%' alt="Handsome Devil" sx={{width: 240, height: 240}}/>
                     </Link>
-                    <Accordion>
-                        <AccordionSummary
-                            expandIcon={<ArrowDropDownIcon />}
-                            aria-controls="panel1-content"
-                            id="panel1-header"
-                        >
-                            Tech Stack
-                        </AccordionSummary>
+            </Item>
+            <Item item>
+                <Link style={{ textDecoration: 'none', color:'black' }} fontSize='2xl' href="mailto:hello@kohldesac.com">
+                        <EmailIcon fontSize='xl'/> hello@kohldesac.com
+            </Link>
+            </Item>
+            </Grid>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    Tech Stack
+                </AccordionSummary>
 
-                        <AccordionDetails>
+                <AccordionDetails>
 
-                        <TechStack />
+                <TechStack />
 
-                        </AccordionDetails>
-                    </Accordion>
-                </Container>
-        </Flex>
+                </AccordionDetails>
+            </Accordion>
+       
+        </Box>
         );
 }
 
