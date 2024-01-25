0b69da4bd9cc4c2c12556a30cb0d7c2743b6e02d
Topher <topher@kohldesac.com>
2024-01-25 11:34:32 -0800

    blah

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,6 +1,8 @@
 import React, { } from 'react';
+import { Stack, Typography } from '@mui/material';
 import htmlSvgIcon from './helpers/images/svgs/html.svg';
 import cssSvgIcon from './helpers/images/svgs/css.svg';
+import { BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoReact, TbBrandReactNative, BiLogoPostgresql, BiLogoGit } from 'react-icons';
 
 
 export default function TechStack() {
@@ -8,7 +10,7 @@ export default function TechStack() {
 
     return (
         <>
-        <img src={htmlSvgIcon} alt="html5" width="150" height="75"/> HTML5
+        {/* <img src={htmlSvgIcon} alt="html5" width="150" height="75"/> HTML5
         <img src={cssSvgIcon} alt="html5" width="150" height="75"/> CSS3
         <img src={htmlSvgIcon} width="150" height="75"/> JavaScript
         <img src={htmlSvgIcon} width="150" height="75"/> React
@@ -17,7 +19,36 @@ export default function TechStack() {
         <img src={htmlSvgIcon} width="150" height="75"/> Webpack
         <img src={htmlSvgIcon} width="150" height="75"/> Git
         <img src={htmlSvgIcon} width="150" height="75"/> NodeJS
-        <img src={htmlSvgIcon} width="150" height="75"/> TypeScript
+        <img src={htmlSvgIcon} width="150" height="75"/> TypeScript */}
+
+        <Stack alignItems='center' padding="1rem">
+                    <BiLogoHtml5 size="5rem" />
+                    <Typography >HTML5</Typography>
+                </Stack>
+                <Stack alignItems='center' padding="1rem">
+                    <BiLogoCss3 size="5rem" />
+                    <Typography alignItems='center'>CSS3</Typography>
+                </Stack>
+                <Stack alignItems='center' padding="1rem">
+                    <BiLogoJavascript size="5rem" />
+                    <Typography alignItems='center'>JavaScript</Typography>
+                </Stack>
+                <Stack alignItems='center' padding="1rem">
+                    <BiLogoReact size="5rem" />
+                    <Typography alignItems='center'>React</Typography>
+                </Stack>
+                <Stack alignItems='center' padding="1rem">
+                    <TbBrandReactNative size="5rem" />
+                    <Typography alignItems='center'>React Native</Typography>
+                </Stack>
+                <Stack alignItems='center' padding="1rem">
+                    <BiLogoPostgresql size="5rem" />
+                    <Typography alignItems='center'>PostgreSQL</Typography>
+                </Stack>
+                <Stack alignItems='center' padding="1rem">
+                    <BiLogoGit size="5rem" />
+                    <Typography alignItems='center' >Git</Typography>
+                </Stack>
         </>
 
      );
