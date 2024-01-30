e5855207ecb26798da495d7213e869cce33d3240
Topher Kohlstedt <topher@kohldesac.com>
2024-01-30 11:26:17 -0800

    added socials and links

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Container, Typography, Box, Link, Avatar,  } from '@mui/material';
+import { Container, Typography, Link, Avatar,  } from '@mui/material';
 import profilepic from './helpers/images/profilepic.png'
 import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import BookIcon from '@mui/icons-material/Book';
 
 export default function Socials() {
 
@@ -10,14 +13,22 @@ export default function Socials() {
         <>
             <Container style={{paddingBottom:'1rem'}}>
                 <Typography variant="h5">Topher Kohlstedt</Typography>
-                    <Link href="http://www.kohldesac.dev">
-                        <Avatar src={profilepic} borderRadius='50%' alt="Handsome Devil" sx={{width: 240, height: 240}}/>
-                    </Link>
-
-
+               
+                    <Avatar src={profilepic} borderRadius='50%' alt="Handsome Devil" sx={{width: 240, height: 240}}/>
+             
                 <Link style={{ textDecoration: 'none', color:'black' }} fontSize='2xl' href="mailto:hello@kohldesac.com">
                         <EmailIcon fontSize='xl'/> hello@kohldesac.com
                 </Link>
+                <Link style={{ textDecoration: 'none', color:'black' }} fontSize='2xl' href="https://github.com/tkohldesac">
+                        <GitHubIcon fontSize='xl'/> GitHub
+                </Link>
+                <Link style={{ textDecoration: 'none', color:'black' }} fontSize='2xl' href="https://www.linkedin.com/in/topher-kohlstedt-73583528b/">
+                        <LinkedInIcon fontSize='xl'/> LinkedIn
+                </Link>
+                <Link href="http://www.kohldesac.dev" style={{ textDecoration: 'none', color:'black' }} >
+                        <BookIcon fontSize='xl'/> Blog
+                </Link>
+
             </Container >
         </>
 
